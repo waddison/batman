81ed549784a8b0bfb15b540f97a03098a1f723b0
waddison <woody.addison@gmail.com>
2018-04-17 08:21:25 -0500

    Initial commit

---
+++ src/main/java/com/example/batman/BatmanApplication.java
@@ -0,0 +1,12 @@
+package com.example.batman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatmanApplication.class, args);
+    }
+}
