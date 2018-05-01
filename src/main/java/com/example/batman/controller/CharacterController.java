9fbba9752f66934bfb8eb53768f1b2634376574a
BluPhx55 <bluphx55@gmail.com>
2018-05-01 05:27:02 -0500

    Finished the find by name api

--- src/main/java/com/example/batman/controller/CharacterController.java
+++ src/main/java/com/example/batman/controller/CharacterController.java
@@ -3,22 +3,20 @@ package com.example.batman.controller;
 import com.example.batman.model.Character;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("marvel/character/")
+@RequestMapping("batman/api/v1/")
 public class CharacterController {
 
     @Value("${marvelhash}")
@@ -29,7 +27,8 @@ public class CharacterController {
     private String marvelts;
 
 
-    @RequestMapping(value = "all", method = RequestMethod.GET)
+
+    @RequestMapping(value = "characters", method = RequestMethod.GET)
     public List<Character> list() throws IOException {
         String marvelURL = "https://gateway.marvel.com:443/v1/public/characters?apikey=" + marvelapikey + "&ts=" + marvelts + "&hash=" + marvelhash;
         RestTemplate restTemplate = new RestTemplate();
@@ -37,7 +36,7 @@ public class CharacterController {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(responseEntity.getBody());
         JsonNode results = root.path("data").path("results");
-        List<Character>characters = new ArrayList<Character>();
+        List<Character>characters = new ArrayList<>();
         for(int i = 0; i < results.size(); i++) {
             Character character = mapper.readValue(results.get(i).toString(), Character.class);
             characters.add(character);
@@ -46,5 +45,17 @@ public class CharacterController {
         return characters;
     }
 
+    @RequestMapping(value="character/{name}", method = RequestMethod.GET)
+    public Character get(@PathVariable String name) throws IOException {
+        String marvelURL = "https://gateway.marvel.com:443/v1/public/characters?apikey=" + marvelapikey + "&ts=" + marvelts + "&hash=" + marvelhash;
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(marvelURL + "&name=" + name, String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(responseEntity.getBody());
+        JsonNode results = root.path("data").path("results");
+        Character character = mapper.readValue(results.get(0).toString(), Character.class);
+
+        return character;
+    }
 
 }
